db690baf1e86cbad1f933674fb8361dbebf56c82
Victor López <joselopez16_99@outlook.com>
2021-03-14 16:57:43 -0600

    SQL de base de datos a usar

---
+++ database/catedra.sql
@@ -0,0 +1,389 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.4
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 14-03-2021 a las 23:54:47
+-- Versión del servidor: 10.1.35-MariaDB
+-- Versión de PHP: 8.0.2
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `catedra`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `caso`
+--
+
+CREATE TABLE `caso` (
+  `caso_id` int(11) NOT NULL,
+  `codigo` varchar(20) NOT NULL,
+  `solicitud_id` int(11) NOT NULL,
+  `caso_descripcion` text NOT NULL,
+  `programador` int(11) NOT NULL,
+  `fecha_limite` date NOT NULL,
+  `fecha_observaciones` date NOT NULL,
+  `tester` int(11) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `estado` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `departamento`
+--
+
+CREATE TABLE `departamento` (
+  `departamento_id` int(11) NOT NULL,
+  `departamento` varchar(30) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `departamento`
+--
+
+INSERT INTO `departamento` (`departamento_id`, `departamento`, `fecha_registro`) VALUES
+(1, 'Finanzas', '2021-03-14 20:58:35'),
+(2, 'Ventas', '2021-03-14 20:58:35'),
+(3, 'Facturación fija', '2021-03-14 20:58:56'),
+(4, 'Facturación móvil', '2021-03-14 20:58:56');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_caso`
+--
+
+CREATE TABLE `estado_caso` (
+  `estado_caso_id` int(11) NOT NULL,
+  `estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_solicitud`
+--
+
+CREATE TABLE `estado_solicitud` (
+  `estado_solicitud_id` int(11) NOT NULL,
+  `estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `estado_solicitud`
+--
+
+INSERT INTO `estado_solicitud` (`estado_solicitud_id`, `estado`) VALUES
+(1, 'Aceptada'),
+(2, 'Rechazada');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estado_usuario`
+--
+
+CREATE TABLE `estado_usuario` (
+  `estado_usuario_id` int(11) NOT NULL,
+  `estado` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `estado_usuario`
+--
+
+INSERT INTO `estado_usuario` (`estado_usuario_id`, `estado`) VALUES
+(1, 'Activo'),
+(2, 'Inactivo');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `observaciones`
+--
+
+CREATE TABLE `observaciones` (
+  `observacion_id` int(11) NOT NULL,
+  `caso` int(11) NOT NULL,
+  `observacion` text NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `solicitud`
+--
+
+CREATE TABLE `solicitud` (
+  `solicitud_id` int(11) NOT NULL,
+  `departamento_id` int(11) NOT NULL,
+  `tipo_solicitud` int(11) NOT NULL,
+  `caso` varchar(50) NOT NULL,
+  `descripcion` text NOT NULL,
+  `observaciones` text,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `estado` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_solicitud`
+--
+
+CREATE TABLE `tipo_solicitud` (
+  `tipo_solicitud_id` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tipo_solicitud`
+--
+
+INSERT INTO `tipo_solicitud` (`tipo_solicitud_id`, `tipo`) VALUES
+(1, 'Sistema nuevo'),
+(2, 'Cambio a sistema actual');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tipo_usuario`
+--
+
+CREATE TABLE `tipo_usuario` (
+  `tipo_id` int(11) NOT NULL,
+  `tipo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `tipo_usuario`
+--
+
+INSERT INTO `tipo_usuario` (`tipo_id`, `tipo`) VALUES
+(1, 'Administrador'),
+(2, 'Jefes de áreas funcionales'),
+(3, 'Empleados de las áreas funcionales'),
+(4, 'Jefe de desarrollo'),
+(5, 'Programadores');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `usuario_id` int(11) NOT NULL,
+  `tipo_usuario` int(11) NOT NULL,
+  `usuario` varchar(50) NOT NULL,
+  `nombres` varchar(50) NOT NULL,
+  `apellidos` varchar(70) NOT NULL,
+  `correo` varchar(200) NOT NULL,
+  `contraseña` varchar(200) NOT NULL,
+  `fecha_registro` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `estado` int(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`usuario_id`, `tipo_usuario`, `usuario`, `nombres`, `apellidos`, `correo`, `contraseña`, `fecha_registro`, `estado`) VALUES
+(1, 1, 'admin', 'root', 'manager', 'root@root.com', 'root', '2021-03-14 20:52:21', 1),
+(2, 1, 'admin2', 'Victor', 'López', 'lopez@gmail.com', '123456', '2021-03-14 20:54:03', 1),
+(5, 1, 'victor', 'Victor', 'López', 'lopez.rivera@gmail.com', '12356', '2021-03-14 20:55:44', 1);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `caso`
+--
+ALTER TABLE `caso`
+  ADD PRIMARY KEY (`caso_id`),
+  ADD KEY `solicitud_id` (`solicitud_id`),
+  ADD KEY `programador` (`programador`),
+  ADD KEY `tester` (`tester`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  ADD PRIMARY KEY (`departamento_id`);
+
+--
+-- Indices de la tabla `estado_caso`
+--
+ALTER TABLE `estado_caso`
+  ADD PRIMARY KEY (`estado_caso_id`);
+
+--
+-- Indices de la tabla `estado_solicitud`
+--
+ALTER TABLE `estado_solicitud`
+  ADD PRIMARY KEY (`estado_solicitud_id`);
+
+--
+-- Indices de la tabla `estado_usuario`
+--
+ALTER TABLE `estado_usuario`
+  ADD PRIMARY KEY (`estado_usuario_id`);
+
+--
+-- Indices de la tabla `observaciones`
+--
+ALTER TABLE `observaciones`
+  ADD PRIMARY KEY (`observacion_id`),
+  ADD KEY `caso` (`caso`);
+
+--
+-- Indices de la tabla `solicitud`
+--
+ALTER TABLE `solicitud`
+  ADD PRIMARY KEY (`solicitud_id`),
+  ADD KEY `departamento_id` (`departamento_id`),
+  ADD KEY `tipo_solicitud` (`tipo_solicitud`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- Indices de la tabla `tipo_solicitud`
+--
+ALTER TABLE `tipo_solicitud`
+  ADD PRIMARY KEY (`tipo_solicitud_id`);
+
+--
+-- Indices de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  ADD PRIMARY KEY (`tipo_id`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`usuario_id`),
+  ADD UNIQUE KEY `usuario` (`usuario`),
+  ADD UNIQUE KEY `correo` (`correo`),
+  ADD KEY `tipo_usuario` (`tipo_usuario`),
+  ADD KEY `estado` (`estado`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `caso`
+--
+ALTER TABLE `caso`
+  MODIFY `caso_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `departamento`
+--
+ALTER TABLE `departamento`
+  MODIFY `departamento_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_caso`
+--
+ALTER TABLE `estado_caso`
+  MODIFY `estado_caso_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_solicitud`
+--
+ALTER TABLE `estado_solicitud`
+  MODIFY `estado_solicitud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `estado_usuario`
+--
+ALTER TABLE `estado_usuario`
+  MODIFY `estado_usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `observaciones`
+--
+ALTER TABLE `observaciones`
+  MODIFY `observacion_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `solicitud`
+--
+ALTER TABLE `solicitud`
+  MODIFY `solicitud_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_solicitud`
+--
+ALTER TABLE `tipo_solicitud`
+  MODIFY `tipo_solicitud_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT de la tabla `tipo_usuario`
+--
+ALTER TABLE `tipo_usuario`
+  MODIFY `tipo_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `usuario_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `caso`
+--
+ALTER TABLE `caso`
+  ADD CONSTRAINT `caso_ibfk_1` FOREIGN KEY (`solicitud_id`) REFERENCES `solicitud` (`solicitud_id`),
+  ADD CONSTRAINT `caso_ibfk_2` FOREIGN KEY (`programador`) REFERENCES `usuarios` (`usuario_id`),
+  ADD CONSTRAINT `caso_ibfk_3` FOREIGN KEY (`tester`) REFERENCES `usuarios` (`usuario_id`),
+  ADD CONSTRAINT `caso_ibfk_4` FOREIGN KEY (`estado`) REFERENCES `estado_caso` (`estado_caso_id`);
+
+--
+-- Filtros para la tabla `observaciones`
+--
+ALTER TABLE `observaciones`
+  ADD CONSTRAINT `observaciones_ibfk_1` FOREIGN KEY (`caso`) REFERENCES `caso` (`caso_id`);
+
+--
+-- Filtros para la tabla `solicitud`
+--
+ALTER TABLE `solicitud`
+  ADD CONSTRAINT `solicitud_ibfk_1` FOREIGN KEY (`departamento_id`) REFERENCES `departamento` (`departamento_id`),
+  ADD CONSTRAINT `solicitud_ibfk_2` FOREIGN KEY (`tipo_solicitud`) REFERENCES `tipo_solicitud` (`tipo_solicitud_id`),
+  ADD CONSTRAINT `solicitud_ibfk_3` FOREIGN KEY (`estado`) REFERENCES `estado_solicitud` (`estado_solicitud_id`);
+
+--
+-- Filtros para la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD CONSTRAINT `usuarios_ibfk_1` FOREIGN KEY (`tipo_usuario`) REFERENCES `tipo_usuario` (`tipo_id`),
+  ADD CONSTRAINT `usuarios_ibfk_2` FOREIGN KEY (`estado`) REFERENCES `estado_usuario` (`estado_usuario_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
