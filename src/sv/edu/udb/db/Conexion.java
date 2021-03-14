73c63f69241ad7a9026f95ffe84e4a62febf6571
Victor López <joselopez16_99@outlook.com>
2021-03-14 14:40:11 -0600

    Clase para establecer conexión a la base de datos

--- src/sv/edu/udb/db/Conexion.java
+++ src/sv/edu/udb/db/Conexion.java
@@ -9,6 +9,44 @@ package sv.edu.udb.db;
  *
  * @author Victor López
  */
+
+import java.sql.*;
+
 public class Conexion {
-    
+    private Connection conexion = null;
+    private Statement s = null;
+    private ResultSet rs = null;
+    private String query = "";
+
+    public Conexion() throws SQLException {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            conexion = DriverManager.getConnection("jdbc:mysql://localhost/catedra", "root", "");
+            
+            s = conexion.createStatement();
+        } catch (ClassNotFoundException e1) {
+            System.out.println("ERROR:No encuentro el driver de la BD: " + e1.getMessage());
+        }
+    }
+
+    public ResultSet getRs() {
+        return rs;
+    }
+
+    public void setRs(String consulta) {
+        try {
+            this.rs = s.executeQuery(consulta);
+        } catch (SQLException e2) {
+            System.out.println("ERROR: Fallo en SQL: " + e2.getMessage());
+        }
+    }
+
+    public void setQuery(String query) throws SQLException {
+        this.s.executeUpdate(query);
+    }
+
+    public void cerrarConexion() throws SQLException {
+        conexion.close();
+    }
 }
