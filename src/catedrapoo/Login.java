b888ae30ae8f29c26baaf49c677918f8cb03035b
Victor López <joselopez16_99@outlook.com>
2021-03-16 22:40:39 -0600

    Codificación y Validación de Login

--- src/catedrapoo/Login.java
+++ src/catedrapoo/Login.java
@@ -9,15 +9,60 @@ package catedrapoo;
  *
  * @author Victor López
  */
-public class Login extends javax.swing.JFrame {
 
+import java.sql.*;
+import javax.swing.JOptionPane;
+import sv.edu.udb.db.Conexion;
+import sv.edu.udb.mdi.*;
+import sv.edu.udb.util.ErrorLog;
+
+public class Login extends javax.swing.JFrame {
+    ResultSet resultado = null;
+    Conexion conex;
+    
+    ErrorLog registroLog = new ErrorLog();
+    
     /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
+        setLocationRelativeTo(null);
+        
+        // btnIngresar.setEnabled(false);
     }
+    
+    private void mostrarVentana(int tipo, String user) {
+        JOptionPane.showMessageDialog(null, "Bienvenido " + user + ".", "Bienvenido", JOptionPane.PLAIN_MESSAGE);
+        
+        switch (tipo) {
+            case 1:
+                new Admin().setVisible(true);
+                this.dispose();
+                break;
 
+            case 2:
+                new JefeAreasFuncionales().setVisible(true);
+                this.dispose();
+                break;
+
+            case 3:
+                new EmpleadoAreasFuncionales().setVisible(true);
+                this.dispose();
+                break;
+
+            case 4:
+                new JefeDesarrollo().setVisible(true);
+                this.dispose();
+                break;
+
+            case 5:
+                new Programadores().setVisible(true);
+                this.dispose();
+                break;
+        }
+    }
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -27,21 +72,132 @@ public class Login extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jLabel1 = new javax.swing.JLabel();
+        lblPassword = new javax.swing.JLabel();
+        txtUsuario = new javax.swing.JTextField();
+        lblUsuario = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JPasswordField();
+        btnIngresar = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setFont(new java.awt.Font("Dialog", 1, 18)); // NOI18N
+        jLabel1.setText("Iniciar sesión");
+
+        lblPassword.setText("Contraseña:");
+        lblPassword.setName("lblPassword"); // NOI18N
+
+        txtUsuario.setName("txtUsuario"); // NOI18N
+        txtUsuario.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtUsuarioKeyPressed(evt);
+            }
+        });
+
+        lblUsuario.setText("Usuario:");
+        lblUsuario.setName("lblUsuario"); // NOI18N
+
+        txtPassword.setName("txtPassword"); // NOI18N
+        txtPassword.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtPasswordKeyPressed(evt);
+            }
+        });
+
+        btnIngresar.setText("Ingresar");
+        btnIngresar.setName("btnIngresar"); // NOI18N
+        btnIngresar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnIngresarActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(135, 135, 135)
+                        .addComponent(jLabel1))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(77, 77, 77)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(lblPassword)
+                            .addComponent(lblUsuario))
+                        .addGap(18, 18, 18)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(btnIngresar)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(txtPassword, javax.swing.GroupLayout.DEFAULT_SIZE, 145, Short.MAX_VALUE)
+                                .addComponent(txtUsuario)))))
+                .addContainerGap(91, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(36, 36, 36)
+                .addComponent(jLabel1)
+                .addGap(47, 47, 47)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(txtUsuario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(lblUsuario))
+                .addGap(18, 18, 18)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(lblPassword)
+                    .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(31, 31, 31)
+                .addComponent(btnIngresar)
+                .addContainerGap(66, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnIngresarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnIngresarActionPerformed
+        String usuario = txtUsuario.getText();
+        String password = txtPassword.getText();
+        
+        int tipoUsuario = 0;
+        
+        if (usuario.equals("") || password.equals("")) {
+            JOptionPane.showMessageDialog(null, "Datos erroneos. Revise su usuario o clave.", "Error al ingresar", JOptionPane.ERROR_MESSAGE);
+        } else {
+            String sql = "SELECT usuario, contraseña, tipo_usuario FROM usuarios WHERE usuario = '" + usuario + "' AND contraseña = '" + password + "' AND estado = 1";
+            System.out.println(sql);
+            try {
+                conex = new Conexion();
+
+                conex.setRs(sql);
+
+                resultado = conex.getRs();
+                
+                resultado.last();
+                
+                if (resultado.getRow() > 0) {
+                    tipoUsuario = resultado.getInt(3);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Usuario o clave incorrecta", "Error al ingresar", JOptionPane.ERROR_MESSAGE);
+                }
+                
+                resultado.close();
+                
+                mostrarVentana(tipoUsuario, usuario);
+            } catch (SQLException ex) {           
+                System.out.println(ex.getMessage()); 
+                // registroLog.registrarLog(Logger.getLogger(Login.class.getName()), ex.getMessage(), Login.class.getName());
+            }
+        }
+    }//GEN-LAST:event_btnIngresarActionPerformed
+
+    private void txtPasswordKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtPasswordKeyPressed
+        
+    }//GEN-LAST:event_txtPasswordKeyPressed
+
+    private void txtUsuarioKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtUsuarioKeyPressed
+        
+    }//GEN-LAST:event_txtUsuarioKeyPressed
 
     /**
      * @param args the command line arguments
@@ -79,5 +235,11 @@ public class Login extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnIngresar;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel lblPassword;
+    private javax.swing.JLabel lblUsuario;
+    private javax.swing.JPasswordField txtPassword;
+    private javax.swing.JTextField txtUsuario;
     // End of variables declaration//GEN-END:variables
 }
