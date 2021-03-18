0460955876fb6c6a5552921084df2d536693a7e6
Victor López <joselopez16_99@outlook.com>
2021-03-17 22:13:50 -0600

    Metodo para validar el email

--- src/sv/edu/udb/util/Validaciones.java
+++ src/sv/edu/udb/util/Validaciones.java
@@ -9,6 +9,20 @@ package sv.edu.udb.util;
  *
  * @author Victor López
  */
+
+import java.util.regex.*;
+
 public class Validaciones {
     // Validaciones necesarias para el ingreso de datos
+    public boolean validarEmail(String email) {
+        Pattern pattern = Pattern.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
+        
+        Matcher mather = pattern.matcher(email);
+        
+        if (mather.find() == true) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
